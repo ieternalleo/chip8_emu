9ad0a99dbbd1604d5d8b2631582920a18a62818c
ieternalleo <jovaun.mcg@outlook.com>
2023-01-26 20:34:23 -0500

    Add remaining stubs for  CPU instructions.

--- chip8/src/instruction.rs
+++ chip8/src/instruction.rs
@@ -40,10 +40,13 @@ lazy_static! {
         ld_vx_byte,
         add_vx_byte,
         op_vx_vy,
-        skip,
-        annn,
-        bnnn,
-        cxkk,]
+        sne_vx_vy,
+        ld_i_nnn,
+        jp_nnn,
+        rnd_vx_kk,
+        drw_vx_vy_n,
+        e_ops,
+        f_ops]
         //map.push(Instruction::new("DXYN", 3, dxyn));
         // covers SKP Vx and SKNP Vx
         //map.push(Instruction::new("EX00", 3, dxyn));
@@ -215,41 +218,52 @@ pub fn sub_vx_vy(chip: &mut Chip8) {
 }
 
 // 8xy6 - SHR Vx {, Vy}
-// Set Vx = Vx SHR 1.
-// If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+// Set Vx = Vy SHR 1.
+// Store the value of register VY shifted right one bit in register VX¹
+// Set register VF to the least significant bit prior to the shift
+// VY is unchanged
+pub fn shr_vx_vy(chip: &mut Chip8) {
+    let x = (chip.curr_op >> 8) & 0xF;
+    let y = (chip.curr_op >> 4) & 0xF;
+    let vy = chip.registers[y as usize];
+    // Check lsb of vy
+    chip.registers[0xF] = vy & 0b1;
 
+    chip.registers[x as usize] = vy >> 1;
+}
 // 8xy7 - SUBN Vx, Vy
 // Set Vx = Vy - Vx, set VF = NOT borrow.
 // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+fn subn_vx_vy(chip: &mut Chip8) {}
 
 // 8xyE - SHL Vx {, Vy}
 // Set Vx = Vx SHL 1.
 // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
-
+fn shl_vx_vy(chip: &mut Chip8) {}
 // 9xy0 - SNE Vx, Vy
 // Skip next instruction if Vx != Vy.
 // The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
-pub fn skip(chip: &mut Chip8) {}
-// Annn - LD I, addr
+pub fn sne_vx_vy(chip: &mut Chip8) {}
+// Annn - LD I, nnn
 // Set I = nnn.
 // The value of register I is set to nnn.
-pub fn annn(chip: &mut Chip8) {
+pub fn ld_i_nnn(chip: &mut Chip8) {
     chip.index_register = chip.curr_op & 0x0FFF;
 }
 
 // Bnnn - JP V0, addr
 // Jump to location nnn + V0
 // The program counter is set to nnn plus the value of V0.
-pub fn bnnn(chip: &mut Chip8) {
+pub fn jp_nnn(chip: &mut Chip8) {
     let _v0 = chip.registers[0] as u16;
-    let _nnn= chip.curr_op & (0x0FFF);
+    let _nnn = chip.curr_op & (0x0FFF);
     chip.program_counter = _nnn + _v0;
 }
 
 // Cxkk - RND Vx, byte
 // Set Vx = random byte AND kk.
 // The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
-pub fn cxkk(chip: &mut Chip8) {
+pub fn rnd_vx_kk(chip: &mut Chip8) {
     let rnd = rand::random::<u8>();
     let kk = chip.curr_op as u8;
     let vx = (chip.curr_op >> 2) & 0x0F;
@@ -258,64 +272,74 @@ pub fn cxkk(chip: &mut Chip8) {
 
 // Dxyn - DRW Vx, Vy, nibble
 // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
-
+fn drw_vx_vy_n(chip: &mut Chip8) {}
 // The interpreter reads n bytes from memory, starting at the address stored in I.
 // These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
 // Sprites are XORed onto the existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0.
 // If the sprite is positioned so part of it is outside the coordinates of the display
 // it wraps around to the opposite side of the screen. See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
 
+fn e_ops(chip: &mut Chip8) {}
+
 // Ex9E - SKP Vx
 // Skip next instruction if key with the value of Vx is pressed.
-
 // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+fn skp_vx(chip: &mut Chip8) {}
 
 // ExA1 - SKNP Vx
 // Skip next instruction if key with the value of Vx is not pressed.
-
 // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+fn sknp_vx(chip: &mut Chip8) {}
+
+fn f_ops(chip: &mut Chip8) {}
 
 // Fx07 - LD Vx, DT
 // Set Vx = delay timer value.
-
 // The value of DT is placed into Vx.
+fn ld_vx_dt(chip: &mut Chip8) {}
 
 // Fx0A - LD Vx, K
 // Wait for a key press, store the value of the key in Vx.
-
 // All execution stops until a key is pressed, then the value of that key is stored in Vx.
+fn ld_vx_k(chip: &mut Chip8) {}
 
 // Fx15 - LD DT, Vx
 // Set delay timer = Vx.
-
 // DT is set equal to the value of Vx.
+fn ld_dt_vx(chip: &mut Chip8) {}
 
 // Fx18 - LD ST, Vx
 // Set sound timer = Vx.
-
 // ST is set equal to the value of Vx.
+fn ld_st_vx(chip: &mut Chip8) {}
 
 // Fx1E - ADD I, Vx
 // Set I = I + Vx.
-
 // The values of I and Vx are added, and the results are stored in I.
+fn add_i_vx(chip: &mut Chip8) {}
 
 // Fx29 - LD F, Vx
 // Set I = location of sprite for digit Vx.
-
 // The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+fn ld_f_vx(chip: &mut Chip8) {}
 
 // Fx33 - LD B, Vx
 // Store BCD representation of Vx in memory locations I, I+1, and I+2.
-
 // The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
+fn ld_b_vx(chip: &mut Chip8) {}
 
 // Fx55 - LD [I], Vx
 // Store registers V0 through Vx in memory starting at location I.
-
 // The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+fn ld_i_vx(chip: &mut Chip8) {}
 
 // Fx65 - LD Vx, [I]
 // Read registers V0 through Vx from memory starting at location I.
-
 // The interpreter reads values from memory starting at location I into registers V0 through Vx.
+fn ld_vx_mem_val(chip: &mut Chip8) {}
+
+
+#[cfg(test)]
+mod tests {
+    
+}
